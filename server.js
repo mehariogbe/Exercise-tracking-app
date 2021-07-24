4276d1042c78a0051ceaf44652643c3fe9fc0e03
Jonathan Flores <jonathanflores@hotmail.com>
2021-07-24 15:42:41 -0700

    did oauth and user schema

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express');
-const app = express();
 const PORT = 3000;
+const morgan = require("morgan");
+const session = require("express-session");
+const passport = require("passport");
+
+require("dotenv").config();
+
+const app = express();
+
+
+//Connect to the MongoDB with Mongoose
+require("./config/database");
+require("./config/passport");
+
+
+//Routes Section
 
 
 
+
+//View Engine Set up
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    session({
+      secret: "SEIRFLEXRocks!",
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
