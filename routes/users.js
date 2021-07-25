22fecc66dc43ea5e7ddf5f61af903a2acdef62dc
Jonathan Flores <jonathanflores@hotmail.com>
2021-07-24 17:55:07 -0700

    added user to newUser to routes

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const usersCtrl = require("../controllers/users");
 
 
-router.get('new', usersCtrl.new);
+router.get('new', usersCtrl.newUser);
 router.get('/', usersCtrl.userIndex);
 router.post('/', usersCtrl.create);
 
