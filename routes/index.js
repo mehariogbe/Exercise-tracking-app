9002c9b26795767172dfca1cf722a6300a20a7f2
Mehari Abraha <mehariogbe@yahoo.com>
2021-07-24 19:30:58 -0500

    add routes and partials

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const passport = require("passport");
+const indexCtrl = require('../controllers/index');
 
-
+router.get('/', indexCtrl.index);
 
 router.get(
     "/auth/google",
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const morgan = require("morgan");
 const session = require("express-session");
 const passport = require("passport");
 
+
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
 require("dotenv").config();
 
 const app = express();
@@ -15,12 +20,15 @@ require("./config/passport");
 
 
 //Routes Section
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 
 
 //View Engine Set up
 app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
